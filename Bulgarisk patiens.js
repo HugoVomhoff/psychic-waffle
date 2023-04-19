e519c2bf17542a79f14f0069c3367590f7b59346
davberTe21C <116351178+davberTe21C@users.noreply.github.com>
2023-04-19 14:39:34 +0200

    Update Bulgarisk patiens.js

--- Bulgarisk patiens.js
+++ Bulgarisk patiens.js
@@ -1,2 +1,12 @@
-alert(1)
-alert("okok")
+let korthög = []
+for (i=0; i < prompt("hur många högar?"); i++) {
+    korthög.push(Math.ceil(Math.random()* 10))
+}
+for (i=0; i < korthög.length; i++){
+    korthög[i] = (korthög[i] - 1)
+}
+korthög.push(korthög.length)
+for (i=0; i < korthög.length; i++){
+    if (korthög[i] == 0)
+    {korthög.pop(i)
+    i -= 1}}

983cfca83082056301cbcbfe2c2a7cc4a8bf935e
Hugo Vomhoff <116350779+HugoVomhoff@users.noreply.github.com>
2023-04-27 09:32:14 +0200

    davids ihopkok

---
+++ Test/test.js
@@ -0,0 +1,10 @@
+function Fåvärde1(){
+    let x = document.getElementById("input").value;
+    console.log(x)
+}
+function Fåvärde2(){
+    let y = document.getElementById("input2").value;
+    console.log (y)
+}
+
+// <button class="tja" id="knapp1" onclick="räserbil()">tryck!</button>
